06a232a564d58e90bdc91a586bd90454d9d87b61
Daliah Maurer <daliah.maurer@gmx.ch>
2025-09-26 16:10:29 +0200

    customize code to dance website

--- app/registration/page.jsx
+++ app/registration/page.jsx
@@ -1,5 +1,3 @@
-import { Markdown } from 'components/markdown';
-
 export const metadata = {
     title: 'Fallback'
 };
@@ -17,9 +15,8 @@ There are advatanges to using Edge Functions directly, such as the ability to ac
 
 export default function FallbackPage() {
     return (
-        <>
-            <h1 className="mb-8">You&apos;ve reached the fallback page.</h1>
-            <Markdown content={explainer} />
-        </>
+        <div className="flex flex-col gap-12 sm:gap-16">
+            <h1 className="mb-8">Registration</h1>
+        </div>
     );
 }
---
+++ app/travelinformation/page.jsx
@@ -0,0 +1,11 @@
+export const metadata = {
+    title: 'Travelinformation'
+};
+
+export default async function Page() {
+    return (
+        <>
+            <h1 className="mb-8">Travelinformation</h1>
+        </>
+    );
+}
